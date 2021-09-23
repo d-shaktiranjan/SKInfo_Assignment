1e6cec1f079f318d2bd02bcc18806ded0f2c1173
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-09-23 20:55:02 +0530

    use bootstrap

--- signup.php
+++ signup.php
@@ -41,15 +41,32 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
         <h2>Signup Here</h2>
         <hr>
         <form action="/skinfo/signup.php" method="POST">
-            <input type="text" name="userName" id="userName" placeholder="Shakti Ranjan Debata"> <br>
-            <input type="email" name="userMail" id="userMail" placeholder="example@mail.com"> <br>
-            <input type="number" name="mobile" id="mobile"> <br>
-            <input type="password" name="password" id="password" required> <br>
-            <input type="password" name="conPassword" id="conPassword" required> <br>
-            <div class="form-check">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="userMail" name="userMail" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Full Name</label>
+                <input type="text" class="form-control" id="userName" name="userName" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="conPassword" name="conPassword"
+                    aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Mobile Number</label>
+                <input type="number" class="form-control" id="mobile" name="mobile" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3 form-check">
                 <h4>Select your hobbies</h4>
                 <input type="checkbox" id="reading" name="hobbies[]" value="reading">
-                <label for="reading"> Reading</label><br>
+                <label for="reading" class="form-check-label"> Reading</label><br>
                 <input type="checkbox" id="photography" name="hobbies[]" value="photography">
                 <label for="photography">Photography</label><br>
                 <input type="checkbox" id="outdoorGame" name="hobbies[]" value="outdoorGame">
@@ -61,12 +78,12 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
                 <input type="checkbox" id="somethingElse" name="hobbies[]" value="somethingElse">
                 <label for="somethingElse"> Something Else</label><br>
             </div>
-            <button type="submit">Submit</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
-</script>
+    </script>
 
 </html>
