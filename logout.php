9b7cb1aaa35fb234e3c0a902f9dacbefde61f341
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-09-25 21:47:51 +0530

    add some design in logout page

--- logout.php
+++ logout.php
@@ -5,3 +5,32 @@ session_unset();
 session_destroy();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>Sk Info</title>
+</head>
+
+<body>
+    <?php include 'parts/navbar.php'?>
+    <div class="container">
+        <h1 class="text-center mt-3">
+            You are signed out!
+        </h1>
+        <div class="d-grid gap-2 col-6 mx-auto">
+            <a href="/skinfo/login.php" class="btn btn-success" type="button">Login again!</a>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+</script>
+
+</html>
